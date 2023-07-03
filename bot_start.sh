a0628cedce59a60f4b2bc8a7f0cfec48ba41ccc0
Tsu-Hao Liu <96331813+Tsu-HaoLiu@users.noreply.github.com>
2023-07-03 11:54:51 -0700

    Update bot_start.sh

--- bot_start.sh
+++ bot_start.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+cd /path/to/discord-util-bot
+
 # update local repo
 git fetch
 git pull origin
 
 # Start the Python script and kill it every 24hs
-timeout -k 24h python main.py
+timeout -k 24h 24h python main.py &
